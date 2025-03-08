a3a8226975eaa32d95e437f92c7f26e7bf2febd0
Seref <seref.keskin@logiwa.com>
2025-03-09 00:34:20 +0300

    Replace WeaviateService with MilvusService in services module for vector database management

--- leadership-content-harvester/src/services/__init__.py
+++ leadership-content-harvester/src/services/__init__.py
@@ -4,14 +4,14 @@ Services module for content harvesting.
 
 from .youtube_service import YouTubeService
 from .transcript_service import TranscriptService
-from .weaviate_service import WeaviateService
+from .milvus_service import MilvusService
 from .embedding_service import EmbeddingService
 from .excel_service import ExcelService
 
 __all__ = [
     "YouTubeService", 
     "TranscriptService", 
-    "WeaviateService",
+    "MilvusService",
     "EmbeddingService",
     "ExcelService"
 ]
