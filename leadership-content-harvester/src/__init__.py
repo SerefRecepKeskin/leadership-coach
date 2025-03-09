13544c81e22a308872bf3a6a89bc70c470f36af8
Seref <seref.keskin@logiwa.com>
2025-03-09 18:32:33 +0300

    Update documentation to reflect change from Weaviate to Milvus vector database

--- leadership-content-harvester/src/__init__.py
+++ leadership-content-harvester/src/__init__.py
@@ -2,5 +2,5 @@
 Leadership Content Harvester package.
 
 This package is responsible for extracting leadership content from YouTube videos
-and storing it in a Weaviate vector database.
+and storing it in a Milvus vector database.
 """
